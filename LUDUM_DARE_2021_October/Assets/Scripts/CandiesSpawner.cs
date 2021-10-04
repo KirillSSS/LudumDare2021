b4e332aa0d1e3078dc2181d4a35a4a0a56bf25d1
KirillSSS <savchuk.kirill.ua@gmail.com>
2021-10-04 23:42:38 +0300

    Added 95 monsters and 132 csndies

--- LUDUM_DARE_2021_October/Assets/Scripts/CandiesSpawner.cs
+++ LUDUM_DARE_2021_October/Assets/Scripts/CandiesSpawner.cs
@@ -6,12 +6,50 @@ public class CandiesSpawner : MonoBehaviour
 {
     [SerializeField] private float spawnDelay;
     [SerializeField] private GameObject candy;
+    [SerializeField] private GameObject[] candySpawner;
 
-     private IEnumerator SpawnCandies(GameObject candy, Vector3 startPos)
+    private Vector3 startPos;
+
+    private void Start()
     {
-        yield return new WaitForSeconds(spawnDelay);
-        Instantiate(candy, startPos, Quaternion.identity);
+        //int i = Random.Range(0, candySpawner.Length);
+
+        //startPos = candySpawner[i].transform.position;
+
+        for(int i = 0; i< candySpawner.Length; i++)
+            Instantiate(candy, candySpawner[i].transform.position, Quaternion.identity);
+
+        startPos.x = 0;
+        startPos.y = 0;
+        startPos.z = 0;
     }
 
-    public void Spawn(GameObject g, Vector3 v) => StartCoroutine(SpawnCandies(g,v));
+    private IEnumerator SpawnCandies()
+    {
+        yield return new WaitForSeconds(spawnDelay);
+        //int[] p;
+        //int o = 0;
+
+        //for (int j = 0; j < candySpawner.Length; j++)
+        //    for (int i = 0; i < GameObject.FindGameObjectsWithTag("Candy").Length; i++)
+        //    {
+        //        if (GameObject.FindGameObjectsWithTag("Candy")[i].transform.position == candySpawner[j].transform.position)
+        //        {
+        //            p[o] = j;
+        //        }
+        //    }
+
+        //for (int j = 0; j < candySpawner.Length; j++)
+        //    for (int i = 0; i < p.Length; i++)
+        //        if (j != p[i])
+        //            Instantiate(candy, startPos, Quaternion.identity);
+
+        for (int i = 0; i < GameObject.FindGameObjectsWithTag("Candy").Length; i++)
+            Destroy(GameObject.FindGameObjectsWithTag("Candy")[i]);
+
+        for (int i = 0; i < candySpawner.Length; i++)
+            Instantiate(candy, candySpawner[i].transform.position, Quaternion.identity);
+    }
+
+    public void Spawn() => StartCoroutine(SpawnCandies());
 }
